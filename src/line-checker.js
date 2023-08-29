2720a22c275002f576c598c1593798d3a5caa024
Jonathan Cheng <joncheng.dev@gmail.com>
2023-08-29 16:01:23 -0700

    pass 1st test for occurrenceArray fxn
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>
    Co-authored-by: Jeremy Josol <jeremyjosol@gmail.com>

--- src/line-checker.js
+++ src/line-checker.js
@@ -1,3 +1,18 @@
-export default function occurenceArray(arrayOfNumbers) {
 
+export default function occurenceArray(arrayOfNumbers) {
+    let result = [];
+    for (let i = 1; i<=9; i++) {
+    let counter = 0;
+    arrayOfNumbers.forEach(function(number){
+        if (number === i){
+            counter += 1;
+        }
+    });
+    result.push(counter);
+    }
+    return result;
 }
+
+
+
+
